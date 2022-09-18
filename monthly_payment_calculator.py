1ab582645dd3401538c040e1097d8a3071d5928d
LMNikoletta <nikoletta.lokinemiklan@gmail.com>
2022-09-18 09:46:24 +0200

    Random tesztek

--- monthly_payment_calculator.py
+++ monthly_payment_calculator.py
@@ -1,6 +1,25 @@
 # calculate monthly payment
 def calculate(p: int, months: int, rate: float):
-    return (rate/12) * (1/(1-(1+rate/12)**(-months)))*p
+    return round((rate/12) * (1/(1-(1+rate/12)**(-months)))*p)
+
+
+if calculate(3000000, 60, 0.12) == 66733:
+  print("true")
+else:
+   print("fals")
+if calculate(4000000, 120, 0.10) == 52860:
+    print("true")
+else:
+    print("fals")
+if calculate(6000000, 120, 0.10)== 79290:
+    print("true")
+else:
+    print("fals")
+if calculate(30, 4, 0.10)== 8:
+    print("true")
+else:
+    print("fals")
+
 
 
 
