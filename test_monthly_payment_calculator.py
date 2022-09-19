8795166e383b0c06695aec6269df8226411a63f7
kobanya <nagy.bela.budapest@gmail.com>
2022-09-19 15:18:23 +0200

    monthly paymant függvény kibontása és teszt

---
+++ test_monthly_payment_calculator.py
@@ -0,0 +1,15 @@
+import unittest
+from unittest.mock import MagicMock
+from monthly_payment_calculator import osszeg
+
+#osszeg = calculate(20,10,1)
+
+def test_mpc_true():
+
+    assert osszeg == 3.0255562468897015
+
+
+
+
+
+
